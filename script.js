24338be5b4be4fbf5ff4d82cd1113e0e6618480d
vdsubs <vd.subs@gmail.com>
2025-08-28 02:10:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const orderBtn = document.getElementById("orderBtn");
+    orderBtn.addEventListener("click", () => {
+        alert("Функция заказа будет подключена позже 🚀");
+    });
+});
